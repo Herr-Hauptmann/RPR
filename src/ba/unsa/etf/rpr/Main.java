44466c8b335146ae35f7a09ceab0c72c52893b09
Herr-Hauptmann <tarik.horoz@gmail.com>
2022-11-13 23:10:15 +0100

    Tutorijal 1 - Zadatak 3

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,27 +1,15 @@
 package ba.unsa.etf.rpr;
 
-import java.util.Scanner;
-
 public class Main {
 
-    public static int sumaCifara(int broj)
-    {
-        int suma = 0;
-        while (broj != 0)
-        {
-            suma+= broj%10;
-            broj/=10;
-        }
-        return suma;
-    }
     public static void main(String[] args) {
-        System.out.println("Unesite broj n: ");
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        for(int i = 1; i <= n; i++)
-        {
-            if (i % sumaCifara(i) == 0)
-                System.out.println(i);
-        }
+	    Sat s = new Sat(15,30,45);
+	    s.ispisi();
+	    s.sljedeci();
+        s.ispisi();
+        s.pomjeriZa(-48);
+        s.ispisi();
+        s.postavi(0,0,0);
+        s.ispisi();
     }
 }
